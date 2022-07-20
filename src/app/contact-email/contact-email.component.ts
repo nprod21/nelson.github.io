175828d16e813f2dcbd87ba0235dd3ccb4ff910e
Nelson <en@Nelsons-iMac.local>
2022-07-20 16:58:56 +0100

    Style change binded box shadow property value

--- src/app/contact-email/contact-email.component.ts
+++ src/app/contact-email/contact-email.component.ts
@@ -12,6 +12,12 @@ export class ContactEmailComponent implements OnInit {
 
   public emailSent: boolean = false;
   public boxShadow: string = "";
+  private boxShadowStyle: string = `0 1px 2px rgba(0,0,0,0.07), 
+                                    0 2px 4px rgba(0,0,0,0.07), 
+                                    0 4px 8px rgba(0,0,0,0.07), 
+                                    0 8px 16px rgba(0,0,0,0.07),
+                                    0 16px 32px rgba(0,0,0,0.07), 
+                                    0 32px 64px rgba(0,0,0,0.07)`;
   public formName: string = "";
   public formEmail: string = "";
   public formSubject: string = "";
@@ -40,7 +46,8 @@ export class ContactEmailComponent implements OnInit {
   ngOnInit(): void {
     /* timer delay for content visibility - to prevent flickering before splash screen */
     setTimeout(() => this.showComponent = true, 2800);
-    setTimeout(() => this.boxShadow = "-1px 1px 30px #313241", 3050);
+    // setTimeout(() => this.boxShadow = "-1px 1px 30px #313241", 3050);
+    setTimeout(() => this.boxShadow = this.boxShadowStyle, 3050);
   }
 
   private validateFormName(): boolean {
