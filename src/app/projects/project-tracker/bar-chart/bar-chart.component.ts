c95cf9517af1972b2175fae609e89ec1970b3e66
Nelson <en@Nelsons-iMac.local>
2022-07-20 17:04:15 +0100

    Feat project tracker bar chart component

---
+++ src/app/projects/project-tracker/bar-chart/bar-chart.component.ts
@@ -0,0 +1,61 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+
+import { ChartService } from '../chart.service';
+import { ChartElement } from '../chart-element';
+
+@Component({
+  selector: 'app-bar-chart',
+  templateUrl: './bar-chart.component.html',
+  styleUrls: ['./bar-chart.component.css']
+})
+export class BarChartComponent implements OnInit {
+
+  @Input() category: string = "priority";
+  @Output() elementSelect = new EventEmitter();
+
+  chartElements: ChartElement[] = [];
+
+  constructor(private chartService: ChartService) { }
+
+  ngOnInit(): void {
+    /* Assigns chart elements returned by chart service to component property
+    && if input category provided is priority and all priority values are present,
+        calls method to order chart elements by predefined priority order (High-->Low) */
+    this.chartElements = this.chartService.getChartElements(this.category);
+    if(this.category == "priority" 
+      && this.chartElements.length == 4) this.orderPriorityChartElements();
+  }
+
+  getPercentage(count: number): number {
+    /* Returns percentage, of provided count, as a number (using total) */
+    return this.chartService.getPercentage(count);
+  }
+
+  getClassName(elementName: string): string {
+    /* Returns formatted className for provided elementName
+    (removes dot/period from project names) */
+    return this.chartService.getClassName(elementName);
+  }
+
+  setFilteredTickets(value: string): void {
+    /* Sets filtered tickets using input category and value arg provided
+    && emits elementSelect event output */
+    this.chartService.setFilteredTickets(this.category, value, false, true);
+    this.elementSelect.emit();
+  }
+
+  orderPriorityChartElements(): void {
+    /* Reorders chart elements by predefined priority order (High-->Low) */
+    let orderedChartElements: ChartElement[] = [];
+    let lowIndex: number = this.chartElements.findIndex(element => element.name === "Low");
+    let mediumIndex: number = this.chartElements.findIndex(element => element.name === "Medium");
+    let highIndex: number = this.chartElements.findIndex(element => element.name === "High");
+
+    orderedChartElements.push(this.chartElements[lowIndex]);
+    orderedChartElements.push(this.chartElements[mediumIndex]);
+    orderedChartElements.push(this.chartElements[highIndex]);
+
+    this.chartElements = orderedChartElements;
+  }
+
+}
