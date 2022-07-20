0ab8b6cccc782eb9dc80b50d137f54528e1353fc
Nelson <en@Nelsons-iMac.local>
2022-07-20 22:43:58 +0100

    Increase visibility set timeout delay by 50ms

--- src/app/contact-email/contact-email.component.ts
+++ src/app/contact-email/contact-email.component.ts
@@ -45,7 +45,7 @@ export class ContactEmailComponent implements OnInit {
 
   ngOnInit(): void {
     /* timer delay for content visibility - to prevent flickering before splash screen */
-    setTimeout(() => this.showComponent = true, 2800);
+    setTimeout(() => this.showComponent = true, 3100);
     // setTimeout(() => this.boxShadow = "-1px 1px 30px #313241", 3050);
     setTimeout(() => this.boxShadow = this.boxShadowStyle, 3050);
   }
