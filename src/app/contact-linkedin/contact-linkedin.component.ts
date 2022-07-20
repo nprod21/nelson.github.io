c6b3228eee9bf3bc4b8b05cd33d7f816d3f52924
Nelson <en@Nelsons-iMac.local>
2022-07-20 22:44:09 +0100

    Increase visibility set timeout delay by 50ms

--- src/app/contact-linkedin/contact-linkedin.component.ts
+++ src/app/contact-linkedin/contact-linkedin.component.ts
@@ -20,8 +20,8 @@ export class ContactLinkedinComponent implements OnInit {
 
   ngOnInit(): void {
     /* timer delay for content visibility - to prevent flickering before splash screen */
-    setTimeout(() => this.showComponent = true, 2800);
-    // setTimeout(() => this.boxShadow = "-1px 1px 30px #313241", 3050);
+    setTimeout(() => this.showComponent = true, 3100);
+    // setTimeout(() => this.boxShadow = "-1px 1px 30px #313241", 3750);
     setTimeout(() => this.boxShadow = this.boxShadowStyle, 3050);
   }
 
